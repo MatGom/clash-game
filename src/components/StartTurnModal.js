ddddf4cf205db77f4e1c0099165c70dbfa4b8050
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-03-06 23:01:01 +0000

    Change styles in StartTurnModal component

--- src/components/StartTurnModal.js
+++ src/components/StartTurnModal.js
@@ -3,11 +3,16 @@ import styles from './StartTurnModal.module.scss';
 import Button from './UI/Button';
 
 const StartTurnModal = ({ currentPlayer, playerOneName, playerTwoName, turnNumber, startCurrentTurn }) => {
+  const backgroundTheme = currentPlayer === 'playerOne' ? 'blue' : 'red';
+  const buttonTheme = currentPlayer === 'playerOne' ? 'sapphire' : 'ruby';
+
   return (
-    <div className={styles.playerTurn}>
-      <h2>{currentPlayer === 'playerOne' ? playerOneName : playerTwoName}</h2>
-      <p>Turn {turnNumber}</p>
-      <Button theme='emerald' size='large' onClick={startCurrentTurn}>Start</Button>
+    <div className={`${styles.playerTurnModal} ${styles[backgroundTheme]}`}>
+      <p className={styles.turnNumber}>Turn {turnNumber}</p>
+      <p className={styles.playerName}>{currentPlayer === 'playerOne' ? playerOneName : playerTwoName}</p>
+      <Button theme={buttonTheme} size='large' onClick={startCurrentTurn}>
+        Start
+      </Button>
     </div>
   );
 };
